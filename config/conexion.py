d5b858189933aada123ab958107e61e64996e37a
Angelopr8 <angeloprada2007@cesanrafael.org>
2025-06-11 14:20:26 -0400

    creamos archivo requerimen

--- config/conexion.py
+++ config/conexion.py
@@ -1,9 +1,9 @@
 import mysql.connector
 conexion = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="1234",
-    database="dbventas"
+    host="b4ggqdsnh1uys4bcrqih-mysql.services.clever-cloud.com",
+    user="ujzqm2pshctfolmk",
+    password="APPNh8U3t8V3t1Abt1ie",
+    database="b4ggqdsnh1uys4bcrqih"
 )
 if conexion.is_connected():
    print("conexion exitosa")
